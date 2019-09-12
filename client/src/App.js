7279f4bfd04ba7d5115ddbe7a31edcab0d3235b2
Justas <ko.justas@gmail.com>
2019-09-12 14:00:31 +0300

    Added logo, styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import imgMeme from './img/womanyellingcat.jpg';
+import TextField from './TextField.js';
 
-function App() {
-	async function getPhoto() {
-		const res = await fetch('http://localhost:4000/');
-		const resData = await res.json();
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			woman_text: 'Meme Generator: YOU CANNOT LEAVE TEXT FIELDS EMPTY',
+			cat_text: 'Me+'
+		};
 	}
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<div>
-					<h1>Create Your Own</h1>
-				</div>
+	render() {
+		async function getPhoto() {
+			const res = await fetch('http://localhost:4000/');
+			const resData = await res.json();
+		}
 
-				<img src={imgMeme} alt="meme" />
-				<button onClick={getPhoto}>Submit</button>
-			</header>
-		</div>
-	);
+		return (
+			<div className="App">
+				<header className="App-header">
+					<div>
+						<h1>Create Your Own Meme Here</h1>
+					</div>
+					<div className="Meme-container">
+						<div className="Text-boxes">
+							<textarea className="Text-field" maxLength="110" />
+							<textarea className="Text-field" maxLength="110" />
+						</div>
+
+						<img src={imgMeme} alt="meme" />
+					</div>
+					<button onClick={getPhoto}>Submit</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ client/src/TextField.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function TextField() {
+	return <textarea className="Text-field" maxLength="110" />;
+}
+
+export default TextField;
