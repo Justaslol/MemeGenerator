9f0be6b1bc285ade90026a6cf26cb276d1cb0c64
Justas <ko.justas@gmail.com>
2019-09-13 19:39:37 +0300

    Working on backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import imgMeme from './img/womanyellingcat.jpg';
-import TextField from './TextField';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import imgMeme from "./img/womanyellingcat.jpg";
+import TextField from "./TextField";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.handleWomanInput = this.handleWomanInput.bind(this);
-		this.handleCatInput = this.handleCatInput.bind(this);
-		this.state = {
-			woman_text: 'Meme Generator: YOU CANNOT LEAVE TEXT FIELDS EMPTY',
-			cat_text: 'Me just testing the Submit button:'
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.handleWomanInput = this.handleWomanInput.bind(this);
+    this.handleCatInput = this.handleCatInput.bind(this);
+    this.state = {
+      woman_text: "Meme Generator: YOU CANNOT LEAVE TEXT FIELDS EMPTY",
+      cat_text: "*Me just testing the Submit button*"
+    };
+  }
 
-	handleWomanInput(textInput) {
-		this.setState({
-			woman_text: textInput
-		});
-	}
+  handleWomanInput(textInput) {
+    this.setState({
+      woman_text: textInput
+    });
+  }
 
-	handleCatInput(textInput) {
-		this.setState({
-			cat_text: textInput
-		});
-	}
+  handleCatInput(textInput) {
+    this.setState({
+      cat_text: textInput
+    });
+  }
 
-	render() {
-		async function getPhoto() {
-			const res = await fetch('http://localhost:4000/');
-			const resData = await res.json();
-		}
+  render() {
+    async function getPhoto() {
+      const res = await fetch("http://localhost:4000/");
+      const resData = await res.json();
+    }
 
-		return (
-			<div className="App">
-				<header className="App-header">
-					<div>
-						<h1>Create Your Own Meme Here</h1>
-					</div>
-					<div className="Meme-container">
-						<div className="Text-boxes">
-							<TextField
-								className="Text-field"
-								text={this.state.woman_text}
-								onInputChange={this.handleWomanInput}
-							/>
-							<TextField
-								className="Text-field"
-								text={this.state.cat_text}
-								onInputChange={this.handleCatInput}
-							/>
-						</div>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <h1>Create Your Own Meme Here</h1>
+          </div>
+          <div className="Meme-container">
+            <div className="Text-boxes">
+              <TextField
+                className="Text-field"
+                text={this.state.woman_text}
+                onInputChange={this.handleWomanInput}
+              />
+              <TextField
+                className="Text-field"
+                text={this.state.cat_text}
+                onInputChange={this.handleCatInput}
+              />
+            </div>
 
-						<img src={imgMeme} alt="meme" />
-					</div>
-					<button onClick={getPhoto}>Submit</button>
-				</header>
-			</div>
-		);
-	}
+            <img src={imgMeme} alt="meme" />
+          </div>
+          <button onClick={getPhoto}>Submit</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.json({
-		success: true
-	});
+// app.get("/", (req, res) => {
+//   res.json({
+//     success: true,
+//     body: req.params
+//   });
+// });
+
+app.get("/", (req, res) => {
+  res.send({
+    woman: req.param("woman"),
+    cat: req.param("cat")
+  });
 });
 
 app.listen(4000, () => {
-	console.log('Listening on PORT 4000');
+  console.log("Listening on PORT 4000");
 });
