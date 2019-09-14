bec97a3ce31a1c97aa46f17ca991a0676cb54e6e
Justas <ko.justas@gmail.com>
2019-09-14 17:29:11 +0300

    jimp

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const axios = require("axios");
+const jimp = require("jimp");
 
 const app = express();
 
@@ -9,18 +10,19 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.json({
-//     success: true,
-//     body: req.params
-//   });
-// });
-
 app.get("/", (req, res) => {
-  res.send({
-    woman: req.param("woman"),
-    cat: req.param("cat")
-  });
+  // res.send({
+  //   woman: req.param("woman"),
+  //   cat: req.param("cat")
+  // });
+  jimp
+    .read("womanyellingcat.jpg")
+    .then(picture => {
+      res.send(picture.greyscale);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
 
 app.listen(4000, () => {
